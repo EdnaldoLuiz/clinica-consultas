4bdcc73a0c2210988aca949a4cb960bfbf843c0e
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-01-14 17:37:33 -0300

    feat: added remove schedules from csv

--- src/main/java/com/ednaldoluiz/core/impl/ConsultaServiceImpl.java
+++ src/main/java/com/ednaldoluiz/core/impl/ConsultaServiceImpl.java
@@ -1,38 +1,128 @@
 package com.ednaldoluiz.core.impl;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
+import java.util.UUID;
 
-import com.ednaldoluiz.app.model.Consulta;
 import com.ednaldoluiz.core.service.ConsultaService;
+import com.ednaldoluiz.utils.FileUtil;
+import com.ednaldoluiz.utils.TerminalUtil;
 
 public class ConsultaServiceImpl implements ConsultaService {
-    private List<Consulta> consultas;
+    
+    public void agendarConsulta() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            String nomeArquivoPacientes = "pacientes.csv";
+            List<String> pacientes = FileUtil.lerArquivo(nomeArquivoPacientes);
 
-    public ConsultaServiceImpl() {
-        this.consultas = new ArrayList<>();
+            if (pacientes.isEmpty()) {
+                System.out.println("Nenhum paciente cadastrado.");
+                return;
+            }
+
+            System.out.println("\nLista de Pacientes:\n");
+            System.out.println("+----+------------------+------------------------+");
+            System.out.println("| Nº |       Nome       |        Telefone        |");
+            System.out.println("+----+------------------+------------------------+");
+
+            for (int numeroPaciente = 1; numeroPaciente < pacientes.size(); numeroPaciente++) {
+                String[] pacienteInfo = pacientes.get(numeroPaciente).split(", ");
+                String nome = pacienteInfo[1];
+                String telefone = pacienteInfo[2];
+
+                System.out.printf("| %-2d | %-16s | %-22s |\n", numeroPaciente, nome, telefone);
+            }
+
+            System.out.println("+----+------------------+------------------------+");
+
+            System.out.print("\nEscolha o número correspondente ao paciente para agendar a consulta: ");
+            int escolhaPaciente = scanner.nextInt();
+
+            if (escolhaPaciente >= 1 && escolhaPaciente <= pacientes.size()) {
+                String pacienteSelecionado = pacientes.get(escolhaPaciente);
+
+                String[] pacienteSelecionadoInfo = pacienteSelecionado.split(", ");
+                String nomePacienteSelecionado = pacienteSelecionadoInfo[1];
+
+                System.out.print("Digite o dia da consulta: ");
+                String diaConsulta = new Scanner(System.in).nextLine();
+
+                System.out.print("Digite a hora da consulta: ");
+                String horaConsulta = new Scanner(System.in).nextLine();
+
+                System.out.print("Digite a especialidade da consulta: ");
+                String especialidadeConsulta = new Scanner(System.in).nextLine();
+
+                String nomeArquivoAgendamentos = "agendamentos.csv";
+                try {
+                    FileWriter fileWriter = new FileWriter(nomeArquivoAgendamentos, true);
+                    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+                    bufferedWriter.write(UUID.randomUUID().toString() + ", " + nomePacienteSelecionado + ", "
+                            + diaConsulta + ", " + horaConsulta + ", "
+                            + especialidadeConsulta);
+                    bufferedWriter.newLine();
+                    bufferedWriter.close();
+
+                    System.out.println(TerminalUtil.GREEN + "Consulta agendada com sucesso." + TerminalUtil.RESET);
+                } catch (IOException e) {
+                    System.err.println("Erro ao escrever no arquivo: " + e.getMessage());
+                }
+            } else {
+                System.out.println("Número inválido. Operação cancelada.");
+            }
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo de pacientes: " + e.getMessage());
+        }
     }
 
     @Override
-    public List<Consulta> listarConsultas() {
-        return new ArrayList<>(consultas);
-    }
+    public void cancelarConsulta() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            String nomeArquivoAgendamentos = "agendamentos.csv";
+            List<String> agendamentos = FileUtil.lerArquivo(nomeArquivoAgendamentos);
 
-    @Override
-    public void marcarConsulta(int numeroPaciente, LocalDateTime dataHora, String especialidade) {
-        // Implementação da marcação de consulta
-    }
+            if (agendamentos.isEmpty()) {
+                System.out.println("Nenhum agendamento cadastrado.");
+                return;
+            }
 
-    @Override
-    public void cancelarConsulta(int numeroConsulta) {
-        // Implementação do cancelamento de consulta
-    }
+            System.out.println("\nLista de Agendamentos para Cancelamento:\n");
+            System.out.println("+----+--------------+----------------+----------------------+");
+            System.out.println("| Nº |   Paciente   |      Data      |         Hora         |");
+            System.out.println("+----+--------------+----------------+----------------------+");
 
-    @Override
-    public boolean consultaExistente(LocalDateTime dataHora) {
-        // Verifica se a consulta já existe na lista
-        return consultas.stream().anyMatch(c -> c.getDataHora().equals(dataHora));
+            for (int numeroAgendamento = 1; numeroAgendamento < agendamentos.size(); numeroAgendamento++) {
+                String[] agendamentoInfo = agendamentos.get(numeroAgendamento).split(", ");
+                String nomePaciente = agendamentoInfo[1];
+                String diaConsulta = agendamentoInfo[2];
+                String horaConsulta = agendamentoInfo[3];
+
+                System.out.printf("| %-2d | %-12s | %-14s | %-20s |\n", numeroAgendamento, nomePaciente, diaConsulta,
+                        horaConsulta);
+            }
+
+            System.out.println("+----+--------------+----------------+----------------------+");
+
+            System.out.print("\nEscolha o número correspondente ao agendamento para cancelar: ");
+            int escolhaAgendamento = scanner.nextInt();
+
+            if (escolhaAgendamento >= 1 && escolhaAgendamento <= agendamentos.size()) {
+                agendamentos.remove(escolhaAgendamento);
+
+                FileUtil.atualizarListaConsultas(nomeArquivoAgendamentos, agendamentos);
+
+                System.out.println(TerminalUtil.GREEN + "Consulta cancelada com sucesso." + TerminalUtil.RESET);
+            } else {
+                System.out.println("Número inválido. Operação cancelada.");
+            }
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo de agendamentos: " + e.getMessage());
+        }
     }
 }
-
--- src/main/java/com/ednaldoluiz/core/impl/PacienteServiceImpl.java
+++ src/main/java/com/ednaldoluiz/core/impl/PacienteServiceImpl.java
@@ -1,32 +1,57 @@
 package com.ednaldoluiz.core.impl;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.UUID;
 
 import com.ednaldoluiz.app.model.Paciente;
 import com.ednaldoluiz.core.service.PacienteService;
+import com.ednaldoluiz.core.validator.PacienteValidator;
+import com.ednaldoluiz.utils.FileUtil;
+import com.ednaldoluiz.utils.TerminalUtil;
 
 public class PacienteServiceImpl implements PacienteService {
-    private List<Paciente> pacientes;
-
-    public PacienteServiceImpl() {
-        this.pacientes = new ArrayList<>();
-    }
 
     @Override
-    public List<Paciente> listarPacientes() {
-        return new ArrayList<>(pacientes);
-    }
+    public void adicionarPaciente() {
+        Scanner scanner = new Scanner(System.in);
+        UUID id = UUID.randomUUID();
 
-    @Override
-    public void cadastrarPaciente(String nome, String telefone) {
-        // Implementação do cadastro de paciente
-    }
+        System.out.print("\nDigite o nome do paciente: ");
+        String nomePaciente = scanner.nextLine();
 
-    @Override
-    public boolean pacienteExiste(String telefone) {
-        // Verifica se o paciente já existe na lista
-        return pacientes.stream().anyMatch(p -> p.getTelefone().equals(telefone));
+        System.out.print("Digite o número de telefone do paciente: ");
+        String telefonePaciente = scanner.nextLine();
+
+        String nomeArquivo = "pacientes.csv";
+
+        try {
+
+            List<String> pacientes = FileUtil.lerArquivo(nomeArquivo);
+
+            if (PacienteValidator.pacienteDuplicado(telefonePaciente, pacientes)) {
+                TerminalUtil.mensagemFormatada(TerminalUtil.RED, "Paciente já cadastrado!");
+                return;
+            }
+
+            FileWriter fileWriter = new FileWriter(nomeArquivo, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            Paciente paciente = new Paciente(id, nomePaciente, telefonePaciente);
+
+            bufferedWriter.write(paciente.getId() + ", " + paciente.getNome() + ", " + paciente.getTelefone());
+            bufferedWriter.newLine();
+            bufferedWriter.close();
+
+            System.out.println("ID do paciente: " + id);
+            TerminalUtil.mensagemFormatada(TerminalUtil.GREEN, "Paciente adicionado com sucesso.");
+        } catch (IOException e) {
+            System.err.println("Erro ao escrever no arquivo: " + e.getMessage());
+        }
     }
 }
 
