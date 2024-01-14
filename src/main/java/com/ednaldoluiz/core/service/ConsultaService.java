aca82f2ee40834b30d779700498381629d5f47f7
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-01-14 17:35:39 -0300

    refactor: update service interface methods

--- src/main/java/com/ednaldoluiz/core/service/ConsultaService.java
+++ src/main/java/com/ednaldoluiz/core/service/ConsultaService.java
@@ -1,13 +1,7 @@
 package com.ednaldoluiz.core.service;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import com.ednaldoluiz.app.model.Consulta;
-
 public interface ConsultaService {
-    List<Consulta> listarConsultas();
-    void marcarConsulta(int numeroPaciente, LocalDateTime dataHora, String especialidade);
-    void cancelarConsulta(int numeroConsulta);
-    boolean consultaExistente(LocalDateTime dataHora);
+
+    void agendarConsulta();
+    void cancelarConsulta();
 }
--- src/main/java/com/ednaldoluiz/core/service/PacienteService.java
+++ src/main/java/com/ednaldoluiz/core/service/PacienteService.java
@@ -1,11 +1,6 @@
 package com.ednaldoluiz.core.service;
 
-import java.util.List;
-
-import com.ednaldoluiz.app.model.Paciente;
-
 public interface PacienteService {
-    List<Paciente> listarPacientes();
-    void cadastrarPaciente(String nome, String telefone);
-    boolean pacienteExiste(String telefone);
+
+    void adicionarPaciente();
 }
