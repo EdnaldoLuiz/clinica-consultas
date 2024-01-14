025f74f3a09b1537509b68ebf6fe0f4d79aa03e2
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-01-14 17:41:56 -0300

    feat: created model classes

--- src/main/java/com/ednaldoluiz/app/model/Consulta.java
+++ src/main/java/com/ednaldoluiz/app/model/Consulta.java
@@ -1,27 +1,60 @@
 package com.ednaldoluiz.app.model;
 
-import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class Consulta {
+
+    private UUID id;
     private Paciente paciente;
-    private LocalDateTime dataHora;
+    private String dia;
+    private String hora;
     private String especialidade;
 
-    public Consulta(Paciente paciente, LocalDateTime dataHora, String especialidade) {
+    public Consulta(UUID id, Paciente paciente, String dia, String hora, String especialidade) {
+        this.id = id;
         this.paciente = paciente;
-        this.dataHora = dataHora;
+        this.dia = dia;
+        this.hora = hora;
         this.especialidade = especialidade;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
     }
 
     public Paciente getPaciente() {
         return paciente;
     }
 
-    public LocalDateTime getDataHora() {
-        return dataHora;
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
     }
 
     public String getEspecialidade() {
         return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
     }
 }
--- src/main/java/com/ednaldoluiz/app/model/Paciente.java
+++ src/main/java/com/ednaldoluiz/app/model/Paciente.java
@@ -1,24 +1,39 @@
 package com.ednaldoluiz.app.model;
 
+import java.util.UUID;
+
 public class Paciente {
 
+    private UUID id;
     private String nome;
     private String telefone;
 
-    public Paciente(String nome, String telefone) {
+    public Paciente(UUID id, String nome, String telefone) {
+        this.id = id;
         this.nome = nome;
         this.telefone = telefone;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
     }
 
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getTelefone() {
         return telefone;
     }
+
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
