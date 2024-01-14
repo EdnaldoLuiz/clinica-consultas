ce4d89ec4d8c69071135a4fa21ad643a6593b529
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-01-14 17:39:40 -0300

    fix: fixed read files bug

--- src/main/java/com/ednaldoluiz/utils/FileUtil.java
+++ src/main/java/com/ednaldoluiz/utils/FileUtil.java
@@ -1,16 +1,33 @@
 package com.ednaldoluiz.utils;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileUtil {
-    public static List<String> lerLinhasArquivo(String nomeArquivo) throws IOException {
-        return Files.readAllLines(Paths.get(nomeArquivo));
+
+    public static List<String> lerArquivo(String nomeArquivo) throws IOException {
+        List<String> linhas = new ArrayList<>();
+        
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(nomeArquivo))) {
+            String linha;
+            while ((linha = bufferedReader.readLine()) != null) {
+                linhas.add(linha);
+            }
+        }
+        return linhas;
     }
 
-    public static void escreverLinhasArquivo(List<String> linhas, String nomeArquivo) throws IOException {
-        Files.write(Paths.get(nomeArquivo), linhas);
+    public static void atualizarListaConsultas(String nomeArquivo, List<String> linhas) throws IOException {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(nomeArquivo))) {
+            for (String linha : linhas) {
+                bufferedWriter.write(linha);
+                bufferedWriter.newLine();
+            }
+        }
     }
 }
